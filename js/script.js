d34a8ef97d6065025474a9059ab9d66b6c3dfa2e
FlorentDlh <florentdela60@gmail.com>
2018-11-28 17:11:41 +0100

    Début header avec début menu trigger

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function mobileMenu() {
+    var x = document.querySelector("#mobileMenu");
+    
+    // if (x.className === "topnav") {
+    //     x.className += " responsive";
+    // } else {
+    //     x.className = "topnav";
+    // }
+}
